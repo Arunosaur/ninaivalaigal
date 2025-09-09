b7aca04e5c1e9876a64e65b00aef7e4bc4cac69b
Arun Swaminathan Rajagopalan <Arun.Rajagopalan@mclaneco.com>
2025-09-09 11:43:45 -0500

    feat(cli): Implement command and output capture in zsh script

--- client/mem0.zsh
+++ client/mem0.zsh
@@ -2,17 +2,49 @@
 
 # This function will run before each command is executed
 mem0_preexec() {
-    # In the future, we will capture the command here
-    # For now, we do nothing
-    return
+    # Get the active recording context from the server
+    local active_context=$(./client/mem0 context active 2>/dev/null)
+
+    # If we are not recording, do nothing
+    if [ -z "$active_context" ]; then
+        return
+    fi
+
+    # Capture the command string
+    _MEM0_COMMAND_STRING=$1
+
+    # Create a temporary file to store the command output
+    _MEM0_OUTPUT_FILE=$(mktemp)
+
+    # Start a script session to capture output
+    script -q "$_MEM0_OUTPUT_FILE"
 }
 
 # This function will run after each command has finished
 mem0_precmd() {
-    # In the future, we will capture the output and exit code here
-    # and then call 'mem0 remember'
-    # For now, we do nothing
-    return
+    # If we are not recording, do nothing
+    if [ -z "$_MEM0_COMMAND_STRING" ]; then
+        return
+    fi
+
+    # Stop the script session
+    exit
+
+    # Capture the exit code of the last command
+    local exit_code=$?
+
+    # Read the output from the temporary file
+    local output=$(cat "$_MEM0_OUTPUT_FILE")
+    rm "$_MEM0_OUTPUT_FILE"
+
+    # Construct the JSON payload
+    local json_payload=$(printf '{ "type": "terminal_command", "source": "zsh_session", "data": { "command": "%s", "output": "%s", "exit_code": %d } }' "$_MEM0_COMMAND_STRING" "$output" "$exit_code")
+
+    # Remember the captured data
+    ./client/mem0 remember "$json_payload"
+
+    # Clear the command string to mark that we are done
+    _MEM0_COMMAND_STRING=""
 }
 
 # Register the hook functions
